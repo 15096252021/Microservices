69e89746488ad2ae0469fa3115eb498ba8c752e2
TYANYU <787008407@qq.com>
2023-02-16 09:32:58 +0800

    modified:   src/api/Login/Login.ts

--- src/api/Login/Login.ts
+++ src/api/Login/Login.ts
@@ -1,6 +1,7 @@
 import request from "@/request";
 import { LoginDataInt } from "@/type/Login/Login";
 import qs from "qs"
+
 export function signin (data:LoginDataInt) {
   return request({
     url: `${import.meta.env.VITE_BASE_URL}/signin`,
